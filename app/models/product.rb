978dd3de5e4f5059c835f869c4e9dcb58dc054db
David T Harris <davidtcharris@gmail.com>
2025-05-06 15:03:46 -0400

    Ch7: Task B: Iteration B1: Add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,19 @@ class Product < ApplicationRecord
 
   # Broadcast any product model changes to any listening clients (pg 138)
   after_commit -> { broadcast_refresh_later_to 'products' }
+
+  validates :title, :description, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+
+  validate :acceptible_image
+
+  def acceptible_image
+    return unless image.attached?
+
+    acceptable_types = [ "image/gif", "image/jpeg", "image.png" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
 end

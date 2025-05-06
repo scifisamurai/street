631ff31ac81c64114b609c087d1dd93ab6c5ce9b
David T Harris <davidtcharris@gmail.com>
2025-05-06 00:31:38 -0400

    Make product view update in real time
    Ieration A3 of the book:
    Agile Web Development with Rails 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Only allow modern browsers supporting webp images, web push, badges,
+  # import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # As per pg 135 of Agile Web Development with Rails 8:
+  # - connect Active Storage to our views
+  # NOTE:
+  # Only needed if we're storing files on disk.
+  # If using the cloud you don't need this line.
+  include ActiveStorage::SetCurrent
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  # Broadcast any product model changes to any listening clients (pg 138)
+  after_commit -> { broadcast_refresh_later_to 'products' }
 end

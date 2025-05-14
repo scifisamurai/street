4b031dd2da33d9c5fc6c8b502929bbd9250c419b
David T Harris <davidtcharris@gmail.com>
2025-05-13 23:26:04 -0400

    D2: Add line_items relationships on product & cart
    Also:
    - Add a before_destroy hook to ensure that
      a product can't be deleted if it has line items.
    - Fix the now broken ProductController#destroy test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
   has_one_attached :image
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   # Broadcast any product model changes to any listening clients (pg 138)
   after_commit -> { broadcast_refresh_later_to 'products' }
@@ -18,6 +21,16 @@ class Product < ApplicationRecord
     acceptable_types = [ "image/gif", "image/jpeg", "image.png" ]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
     end
   end
 end

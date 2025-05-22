3991188eb4102bbaec28c11268882c75f748a10c
David T Harris <davidtcharris@gmail.com>
2025-05-21 21:07:05 -0400

    Ch 13 H1: get shipped & received mailers working
    NOTE: As per the book, specifically right before the `Testing Email`
          section we delete shipped.text.erb. This is also needed
          since if we don't do this our order_mailer test fails for this :(.

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,19 +1,31 @@
+#---
+# Excerpted from "Agile Web Development with Rails 8",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/rails8 for more book information.
+#---
 require "test_helper"
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal [ "dave@example.org" ], mail.to
+    assert_equal [ "depot@example.com" ], mail.from
+    assert_match /1 x The Pragmatic Programmer/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal [ "dave@example.org" ], mail.to
+    assert_equal [ "depot@example.com" ], mail.from
+    assert_match %r{
+      <td[^>]*>1<\/td>\s*
+      <td>&times;<\/td>\s*
+      <td[^>]*>\s*The\sPragmatic\sProgrammer\s*</td>
+    }x, mail.body.to_s
   end
 end

75f27a9422aec101f0c40825733ab3a6505aa7b3
David T Harris <davidtcharris@gmail.com>
2025-05-26 23:10:32 -0400

    Ch 13 H2 OT: Add mailcatcher settings
    Update development config to leverage mailcatcher
    for sending emails while in development mode.
    
    NOTES:
    1. This is not part of the book. The book sugests a different method.
    2. You do need to `gem install mailcatcher` if you're testing this
       manually since mailcatcher is not in the Gemfile (as per the
       documentation on https://mailcatcher.me).

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,13 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+
+  # Set settings as per mailcatcher documentation for local catching of emails
+  # https://mailcatcher.me/
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -39,6 +46,9 @@ Rails.application.configure do
 
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

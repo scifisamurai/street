b4618bc300ea08cf60a61510f7b61fafa3baa7a0
David T Harris <davidtcharris@gmail.com>
2025-05-27 06:27:47 -0400

    Ch 14 I2: Add admin index resources
    Add admin index resources
    - index controller/route/view & controller test
    - update authentication concern so that after the admin
      logs in they're taken to the admin index route

---
+++ test/controllers/admin_controller_test.rb
@@ -0,0 +1,13 @@
+
+require "test_helper"
+
+class AdminControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    login_as users(:one)
+  end
+
+  test "should get index" do
+    get admin_url
+    assert_response :success
+  end
+end

d40cef83790269fca37d6c7c3c0b0ea36019b1cc
David T Harris <davidtcharris@gmail.com>
2025-05-26 22:47:13 -0400

    Ch 13 H2: Flesh out the ChargeOrderJob & call it
    NOTE: Since the job calls Order#charge! which already
          is set up to deliver the received email we no longer need it
          in OrdersController#create.
    
    Also:
    - Add system test to ensure the job is run when we a user orders
      something.

--- app/jobs/charge_order_job.rb
+++ app/jobs/charge_order_job.rb
@@ -1,7 +1,8 @@
+# ActiveJob job
 class ChargeOrderJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(order, pay_type_params)
+    order.charge!(pay_type_params)
   end
 end

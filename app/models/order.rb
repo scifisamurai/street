6503104d7f8121938fb4d092c182bd77a596d5b7
David T Harris <davidtcharris@gmail.com>
2025-05-28 07:14:00 -0400

    Ch 16 K2: Creating support requests from emails
    Add code to support:
    - linking orders and support request models
    - generating a support request from an email delivered to
      support@example.com
    -- also add tests for this

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,6 +15,7 @@ class Order < ApplicationRecord
     "Purchase order" => 2
   }
   has_many :line_items, dependent: :destroy
+  has_many :support_requests, dependent: :nullify
   # ...
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys

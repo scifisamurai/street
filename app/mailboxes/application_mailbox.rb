0c8c938ef50a46e6325bb23154025afa29b0bd8c
David T Harris <davidtcharris@gmail.com>
2025-05-28 06:27:44 -0400

    Ch 16 K1: bin/rails g mailbox support
          create  app/mailboxes/support_mailbox.rb
          invoke  test_unit
          create    test/mailboxes/support_mailbox_test.rb
    
    Also:
      Update support_mailbox as per the book to add debugging to
      #process
    
    NOTE: You can manage your inbox locally by going to:
    http://localhost:3000/rails/conductor/action_mailbox/inbound_emails
    Yes, this route does show up in `bin/rails routes`

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,3 +1,7 @@
 class ApplicationMailbox < ActionMailbox::Base
   # routing /something/i => :somewhere
+
+  # Any email to (or CC'd to) support@example.com
+  # should be handles by the class: SupportMailbox
+  routing "support@example.com" => :support
 end

498f03a0553910f225598d4b24c6af30eee805c5
David T Harris <davidtcharris@gmail.com>
2025-05-28 06:44:58 -0400

    Ch 16 K2: bin/rails g model support_request
          invoke  active_record
          create    db/migrate/20250528104437_create_support_requests.rb
          create    app/models/support_request.rb
          invoke    test_unit
          create      test/models/support_request_test.rb
          create      test/fixtures/support_requests.yml
    
    Then update the migration above as per the book and run it.
    NOTE: The comments in the migration do not appear to be stored
          in sqlite3.  I ran a query to check the schema and the comments
          aren't present :(
    
    I ran the following as per
    forum.xojo.com/t/documenting-the-sqlite-data-base-tables/68781/10
    
    `select sql from sqlite_schema where name = 'support_requests';`
    and got:
    
    ```sql
    CREATE TABLE "support_requests" (
      "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
      "email" varchar,
      "subject" varchar,
      "body" text,
      "order_id" integer,
      "created_at" datetime(6) NOT NULL,
      "updated_at" datetime(6) NOT NULL,
      CONSTRAINT "fk_rails_ec665a3e09"+
      FOREIGN KEY ("order_id")
        REFERENCES "orders" ("id")
    )
    (1 row)
    ```
    
    Also, if you tail log/development.log you'll see
    similar log output.
    Reference: https://stackoverflow.com/questions/4100512/
      is-it-possible-to-output-the-sql-change-scripts-that-rake-
      dbmigrate-produces
    
    TODO See what happens if I run a similar migration
    in PostgreSQL (which should support column comments).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_28_101004) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_28_104437) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.string "message_id", null: false
@@ -92,6 +92,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_28_101004) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "support_requests", force: :cascade do |t|
+    t.string "email"
+    t.string "subject"
+    t.text "body"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_support_requests_on_order_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email_address", null: false
@@ -107,4 +117,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_28_101004) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "sessions", "users"
+  add_foreign_key "support_requests", "orders"
 end
